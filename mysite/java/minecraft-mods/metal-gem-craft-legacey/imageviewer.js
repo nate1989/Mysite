e1aca3fa9f3bd4ddd3206e0d868c5822988922c5
nate1989 <njohnson1989@aol.com>
2022-03-13 06:03:45 -0400

    update

---
+++ mysite/java/minecraft-mods/metal-gem-craft-legacey/imageviewer.js
@@ -0,0 +1,126 @@
+function TYframe(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "block"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "none"
+	}
+function image1(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Copper.png)"
+	}
+function image2(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Tin.jpg)"
+	}
+function image3(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Bronze.jpg)"
+	}
+function image4(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Steel.jpg)"
+	}
+function image5(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Silver.jpg)"
+	}
+function image6(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Mithril.jpg)"
+	}
+function image7(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/Nether-Iron.jpg)"
+	}
+function image8(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url(https://raw.githubusercontent.com/nate1989/Mysite/main/mysite/css/minecraft-mods/metal-gem-craft-legacey/backgrounds/WitherGem.jpg)"
+	}
+function image9(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url()"
+	}
+function image10(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url()"
+	}
+function image11(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url()"
+	}
+function image12(){
+	var ef1 = document.getElementById("frame1");
+	ef1.style.display = "none"
+	var ef2 = document.getElementById("frame2");
+	ef2.style.display = "block"
+	ef2.style.backgroundImage = "url()"
+	}
+function image13(){
+	var ef1 = document.getElementById("adbar");
+	ef1.style.display = "none"
+	}
+function adbar(){
+	var ef1 = document.getElementById("adbar");
+	ef1.classList.remove("adbar")
+	ef1.classList.add("HQQrAh0HHQIdBwIE")
+	var ef2 = document.getElementById("adgrid1");
+	ef2.classList.remove("adgrid")
+	ef2.classList.add("GHQQrAh0HHQIdBwIE")
+	var ef3 = document.getElementById("adgrid2");
+	ef3.classList.remove("adgrid")
+	ef3.classList.add("GHQQrAh0HHQIdBwIE")
+	var ef4 = document.getElementById("adgrid3");
+	ef4.classList.remove("adgrid")
+	ef4.classList.add("GHQQrAh0HHQIdBwIE")
+	document.getElementById("adbar").id = "HQQrAh0HHQIdBwIEs"; 
+	document.getElementById("adgrid1").id = "SHQQrAh0HHQIdBwIE1"; 
+	document.getElementById("adgrid2").id = "SHQQrAh0HHQIdBwIE2"; 
+	document.getElementById("adgrid3").id = "SHQQrAh0HHQIdBwIE3"; 
+	var ef1 = document.getElementById("sidebar");
+	ef1.classList.remove("sb1")
+	ef1.classList.add("sidebar")
+	var ef2 = document.getElementById("sidegrid1");
+	ef2.classList.remove("sb1")
+	ef2.classList.add("sidegrid")
+	var ef3 = document.getElementById("sidegrid2");
+	ef3.classList.remove("sb1")
+	ef3.classList.add("sidegrid")
+	var ef4 = document.getElementById("sidegrid3");
+	ef4.classList.remove("sb1")
+	ef4.classList.add("sidegrid")
+	var ef5 = document.getElementById("sidebarbutton");
+	ef5.style.color = "#408000"
+	}
